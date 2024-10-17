224390d9bbfb4046b04239ca53fd97974dbb06de
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-10-17 20:54:42 -0300

    finally have a layout

--- restore-workspace.sh
+++ restore-workspace.sh
@@ -1,6 +1,11 @@
-# !/bin/sh
+#!/bin/sh
 
 for layout in ~/.config/i3/layouts/*; do
     i3-msg "workspace $(basename "$layout" .json); append_layout $layout"
 done
 
+(/usr/bin/x-terminal-emulator &)
+(/usr/bin/x-terminal-emulator &)
+(/usr/bin/firefox &)
+(/usr/bin/firefox &)
+(/usr/bin/Discord &)
