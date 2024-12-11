f1fc937cc1ab4c04f9b5cf0275dbffeb70bb3b41
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-12-10 22:40:11 -0300

    Add: screencap shortcut and changed startup setup

--- restore-workspace.sh
+++ restore-workspace.sh
@@ -4,8 +4,8 @@ for layout in ~/.config/i3/layouts/*; do
     i3-msg "workspace $(basename "$layout" .json); append_layout $layout"
 done
 
-(/usr/bin/x-terminal-emulator &)
-(/usr/bin/x-terminal-emulator &)
-(/usr/bin/firefox &)
-(/usr/bin/firefox &)
-(/usr/bin/Discord &)
+(/usr/bin/x-terminal-emulator &) #1
+(/usr/bin/x-terminal-emulator &) #2
+(/usr/bin/google-chrome &) #3
+(/usr/bin/google-chrome &) #9
+(/usr/bin/Discord &) #10
